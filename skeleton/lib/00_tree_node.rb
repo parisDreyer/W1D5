b5697a4cbdd3fac25642bb3b258a79f167d49a54
Your Name <you@example.com>
2018-08-24 15:04:54 -0700

    Add poly tree and DFS

---
+++ skeleton/lib/00_tree_node.rb
@@ -0,0 +1,53 @@
+class PolyTreeNode
+  
+  def initialize(value, parent = nil, children = [])
+    @value = value
+    @parent = parent # instance variables AKA properties
+    @children = children
+  end
+  
+  def parent=(other_node)
+    if @parent 
+      @parent.children.delete(self) 
+    end
+    @parent = other_node
+    if @parent != nil
+      @parent.children.push(self) unless @parent.children.include?(self)
+    end
+  end
+  
+  def parent 
+    @parent
+  end 
+  
+  def children 
+    @children
+  end 
+  
+  def value
+    @value
+  end 
+  
+  def add_child(child_node)
+    child_node.parent = self 
+  end
+  
+  def remove_child(child_node)
+    if children.include?(child_node) 
+      child_node.parent = nil
+    else
+      raise "Not a child of this tree node."
+    end
+  end
+  
+  def dfs(target_value)
+    return self if self.value == target_value
+    children.each do |node|
+      returned_node = node.dfs(target_value)
+      if returned_node 
+        return returned_node if target_value == returned_node.value
+      end
+    end
+    nil
+  end
+end
